babd94b385bf3a26f303ad0bf0840cf37a2672e1
Vl-Mikhail <evil_bear@mail.ru>
2017-09-13 22:33:04 +0700

    finish app

--- swoosh/Controller/SkillVC.swift
+++ swoosh/Controller/SkillVC.swift
@@ -9,27 +9,28 @@
 import UIKit
 
 class SkillVC: UIViewController {
+    
+    var player: Player!
+    @IBOutlet weak var finishBtn: BorderButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onFinishTapped(_ sender: Any) {
+        performSegue(withIdentifier: "HomeVCSegue", sender: self)
     }
-    */
-
+    
+    @IBAction func onBeginnerTapped(_ sender: Any) {
+        selectSkill(skillType: "beginner")
+    }
+    
+    @IBAction func onBallerTapped(_ sender: Any) {
+        selectSkill(skillType: "baller")
+    }
+    
+    func selectSkill(skillType: String) {
+        player.selectedSkillLevel = skillType
+        finishBtn.isEnabled = true
+    }
 }
---
+++ swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  swoosh
+//
+//  Created by Mikhail on 10.09.17.
+//  Copyright © 2017 Mikhail. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var disirdLeague: String?
+    var selectedSkillLevel: String?
+}
